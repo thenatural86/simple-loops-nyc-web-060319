6812020ae6f4c85f2b1d412d70c13be6f1b37cf6
The Natural <philip.smalls86@gmail.com>
2019-05-19 03:25:29 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,9 +21,9 @@ def loop_iterator(number_of_times)
 end
 
 
+# code your solution here using the "times" keyword
 
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
