24336b0cafb89240154ccc5b3789b41a97110a8f
The Natural <philip.smalls86@gmail.com>
2019-05-19 03:40:54 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -59,7 +59,7 @@ end
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 1..number_of_times
-  for counter in 
+  for item in counter 
   
 end
 
