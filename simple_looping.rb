b5f3020e51478351fc8d3611d7432f56721c269f
The Natural <philip.smalls86@gmail.com>
2019-05-19 02:51:12 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,7 +10,7 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do
-    integer += 0 
+    integer += 1  
     puts #{phrase}
   break
   end
